d63111712d286cb581ec98bbe338e39d5b44c2da
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-01 20:53:13 -0700

    added onSaveInstanceState

--- app/src/main/java/com/hanseltritama/onsaveinstancestatedemo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/onsaveinstancestatedemo/MainActivity.kt
@@ -2,11 +2,33 @@ package com.hanseltritama.onsaveinstancestatedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var num = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = num.toString()
+
+        button.setOnClickListener {
+            num++
+            textView.text = num.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("key_integer", num)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        num = savedInstanceState.getInt("key_integer", 0)
+        textView.text = num.toString()
     }
 }
